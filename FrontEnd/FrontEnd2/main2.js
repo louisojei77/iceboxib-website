574e9eab3649762dca8c750e6c84ade413d61dbd
louisojei77 <110875611+louisojei77@users.noreply.github.com>
2022-08-14 02:06:34 +0100

    addedfile

--- FrontEnd/FrontEnd2/main2.js
+++
@@ -1,8 +0,0 @@
-const notify_btn = document.querySelector('.notifyicon');
-const notify_msg = document.querySelector('.message-nav');
-
-notify_btn.addEventListener('click', function () {
-    notify_btn.classList.add('is-active');
-    notify_msg.classList.add('is-active');
-});
-
